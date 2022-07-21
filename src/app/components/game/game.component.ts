15e3561b181d20f6fcfdce42e433095c02904511
paulreitz <avenose@gmail.com>
2022-07-20 18:32:36 -0700

    Project Layout: Load and display game in game component

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { MainGame } from '../../game-canvas/main-game';
 
 @Component({
     selector: 'app-game',
     templateUrl: './game.component.html',
     styleUrls: ['./game.component.scss'],
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements AfterViewInit {
+    canvasId: string = 'game-canvas';
+    game: MainGame;
     constructor() {}
-
-    ngOnInit(): void {}
+    ngAfterViewInit(): void {
+        const gameConfig: Phaser.Types.Core.GameConfig = {
+            type: Phaser.AUTO,
+            scale: {
+                parent: this.canvasId,
+                mode: Phaser.Scale.NONE,
+                autoCenter: Phaser.Scale.CENTER_BOTH,
+                width: 800,
+                height: 640,
+            },
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: { y: 500 },
+                    debug: environment.showDebug,
+                },
+            },
+        };
+        this.game = new MainGame(gameConfig);
+    }
 }
